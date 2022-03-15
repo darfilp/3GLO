23e13c9e8da8b8021a6b938bee9ee84001ced17f
Daria <wwdarcool@yandex.ru>
2022-03-15 17:15:45 +0300

    add 1 task

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import one from "../src/modules/one";
+import two from "../src/modules/two";
+
+
+one();
+two();
---
+++ src/modules/two.js
@@ -0,0 +1,6 @@
+function two() {
+    console.log('two')
+    console.log('two')
+};
+
+export default two
