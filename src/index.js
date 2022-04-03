cf287c5ffb2b2fd4bf40927537ec40651af07af9
Daria <wwdarcool@yandex.ru>
2022-04-03 21:14:34 +0300

    add task 23

--- src/index.js
+++ src/index.js
@@ -3,14 +3,11 @@ import menu from "./modules/menu";
 import modal from "./modules/modal";
 import form from "./modules/form";
 import tabs from  "./modules/tabs";
-import slider from "./modules/slider"
+import slider from "./modules/slider";
+import calc from "./modules/calc"
 
-    // const createDots = () => {
-    //     console.log(dotsContainer)
-    //     const dot = document.createElement('li');
-    //     //dot.classList.add('dot');
-    //     //dot.append();
-    // };
+
+//price
 
 tabs();
 timer('2022-04-16');
@@ -18,4 +15,5 @@ menu();
 modal();
 form();
 slider();
+calc(100);
 
---
+++ src/modules/calc.js
@@ -0,0 +1,58 @@
+const calc = (price = 100) => {
+  const inputNumber = document.querySelectorAll(".calc-block input");
+  const calcBlock = document.querySelector(".calc-block");
+  const calcType = document.querySelector(".calc-type");
+  const calcSquare = document.querySelector(".calc-square");
+  const calcCount = document.querySelector(".calc-count");
+  const calcDay = document.querySelector(".calc-day");
+  const total = document.getElementById("total");
+
+  const regExpNumber = /[^0-9]+/i;
+
+  const countCalc = () => {
+    const calcTypeValue = +calcType.options[calcType.selectedIndex].value;
+    const calcSquareValue = calcSquare.value;
+
+    let totalValue = 0;
+    let calcCountValue = 1;
+    let calcDayValue = 1;
+
+    if (calcCount.value > 1) {
+      calcCountValue += +calcCount.value / 10;
+    }
+
+    if (calcDay.value && calcDay.value < 5) {
+      calcDayValue = 2;
+    } else if (calcDay.value && calcDay.value < 10) {
+      calcDayValue = 1.5;
+    }
+
+    if (calcType.value && calcSquare.value) {
+      totalValue =
+        price * calcTypeValue * calcSquareValue * calcCountValue * calcDayValue;
+    } else {
+      totalValue = 0;
+    }
+
+    total.textContent = Math.floor(totalValue);
+  };
+
+  inputNumber.forEach((input) => {
+    input.addEventListener("input", (e) => {
+      e.target.value = e.target.value.replace(regExpNumber, "");
+    });
+  });
+
+  calcBlock.addEventListener("input", (e) => {
+    if (
+      e.target === calcType ||
+      e.target === calcSquare ||
+      e.target === calcCount ||
+      e.target === calcDay
+    ) {
+      countCalc();
+    }
+  });
+}
+
+export default calc
