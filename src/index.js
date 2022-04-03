65acd44d882c3ef1c4a00e3d76ab62547046aa57
Daria <wwdarcool@yandex.ru>
2022-04-03 20:59:52 +0300

    add task 23

--- src/index.js
+++ src/index.js
@@ -3,9 +3,19 @@ import menu from "./modules/menu";
 import modal from "./modules/modal";
 import form from "./modules/form";
 import tabs from  "./modules/tabs";
+import slider from "./modules/slider"
+
+    // const createDots = () => {
+    //     console.log(dotsContainer)
+    //     const dot = document.createElement('li');
+    //     //dot.classList.add('dot');
+    //     //dot.append();
+    // };
 
 tabs();
 timer('2022-04-16');
 menu();
 modal();
 form();
+slider();
+
