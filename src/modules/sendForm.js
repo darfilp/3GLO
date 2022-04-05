9b94151e7d63f944c7db8ae5868084ab498123a9
Daria <wwdarcool@yandex.ru>
2022-04-05 18:19:33 +0300

    fix bag

--- src/modules/sendForm.js
+++ src/modules/sendForm.js
@@ -1,20 +1,38 @@
+let flag = true;
+
 const sendForm = ({
     formID,
     someElem = []
    }) => {
 
   const form = document.getElementById(formID);
-  const allInputs = document.querySelectorAll('input')
-  const allSelects = document.querySelectorAll('select')
-  const statusBlock = document.createElement('div')
-  const loadText = 'Загрузка...'
-  const errorText = 'Ошибка...'
-  const successText = 'Спасибо!Наш менеджер с вами свяжется.'
+  const statusBlock = document.createElement("div");
+  const loadText = "Загрузка...";
+  const errorText = "Ошибка...";
+  const successText = "Спасибо наш менеджер свяжется с вами!";
+
+  
+  const validateInputs = (input) => {
+    if ((input.name === "user_name" && input.value.length > 2) || (input.name === "user_email" && input.value !== "") || (input.name === "user_phone" && (input.value.length < 13)) || (input.name === "user_message")) {
+        flag = false;          
+    } else {
+        statusBlock.textContent = errorText;
+    }
+  };
 
   const validate = (list) => {
-    let success = true
-    return success
-  };
+    let success = true; 
+    
+    list.forEach(input => {
+
+      validateInputs(input);
+
+      if (flag === false) {
+        success = false;
+      }
+    });
+    return success;
+  };  
 
   const sendData = (data) => {
     return fetch('https://jsonplaceholder.typicode.com/posts', {
@@ -22,84 +40,63 @@ const sendForm = ({
       body: JSON.stringify(data),
       headers: {
         "Content-Type": "application/json"
-      }
-    }).then(res => res.json())
-  }
+      },
+    }).then(res => res.json());
+  };
 
   const submitForm = () => {
-    const formElements = form.querySelectorAll('input')
-    const formData = new FormData(form)
-    const formBody = {}
+    const formElements = form.querySelectorAll('input');
+    const formData = new FormData(form);
+    const formBody = {};
+
+    statusBlock.textContent = loadText;
+    form.append(statusBlock);
 
     formData.forEach((val, key) => {
-      formBody[key] = val
-    })
+      formBody[key] = val;
+    });
 
     someElem.forEach(elem => {
-      const element = document.getElementById(elem.id)
-
+      const element = document.getElementById(elem.id);
       if (elem.type === 'block') {
-        formBody[elem.id] = element.textContent
+        formBody[elem.id] = element.textContent;
       } else if (elem.type === 'input') {
-        formBody[elem.id] = element.value
+        formBody[elem.id] = element.value;
       }
-    })
+    });
 
     if (validate(formElements)) {
       sendData(formBody)
-        .then(data => {
-          statusBlock.textContent = successText
-
+        .then(data => {          
+          statusBlock.textContent = successText;
           formElements.forEach(input => {
-            input.value = ''
+            input.value = '';
           });
-
-          [...allInputs, ...allSelects].forEach(input => input.value = '')
-
-          someElem.forEach(elem => {
-            const element = document.getElementById(elem.id)
-            if (elem.type === 'block') {
-              element.textContent = '0'
-            } else if (elem.type === 'input') {
-              element.value = ''
-            }
-          })
-
-          setTimeout(() => {
-            form.removeChild(statusBlock);
-          }, 5000);
         })
         .catch(error => {
-          statusBlock.textContent = errorText
-          setTimeout(() => {
-            form.removeChild(statusBlock);
-          }, 5000);
-
-        })
-    } else {
-      return alert('Данные не валидны')
+          statusBlock.textContent = errorText;
+        });         
+    } else {      
+      alert('Проверьте данные');
     }
-
-    statusBlock.textContent = loadText
-    form.append(statusBlock)
-  }
+  };
 
   statusBlock.style.color = '#19b5fe'
 
+
   try {
     if (!form) {
-      throw new Error('Ошибка!')
+      throw new Error('Ошибка!');
     }
-
-    form.addEventListener('submit', (event) => {
-      event.preventDefault()
-
-      submitForm()
+    form.addEventListener('submit', (e) => {
+      e.preventDefault();
+      submitForm();
       console.log('Отправлено');
-    })
+    });
   } catch (error) {
-    console.log(error.message);
+    console.log(error.massage);
   }
-}
+
+};
 
 export default sendForm;
